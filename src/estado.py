45a14884af3b22341162f096f6da51c35bfd46b4
Gabriel Ribeiro Bizerril <gabrielribeirob.16@gmail.com>
2025-12-18 10:46:41 -0300

    Update estado.py

--- src/estado.py
+++ src/estado.py
@@ -2,22 +2,33 @@ from config import settings
 from pathlib import Path
 from utils import file_io
 import pandas as pd
+from transform import sanitize
 pd.set_option('display.max_columns', None)
 
 ESTADO_FILE_NAME = Path(settings.FINAL_DATA_PATH/settings.ESTADO_V2_FILE_NAME)
 
 if __name__=='__main__':
+    # Carregamento da base
     df = file_io.load_to_dataframe(ESTADO_FILE_NAME)
-    # RESHAPE
+
+    # Mantém apenas as colunas de interesse
     df = df[settings.COLUNAS_INTERESSE_ESTADO]
-    # SANITIZE
-    df = df.rename(columns=RENAME_COLUNAS_ESTADO)
-    # SANITIZE
+
+    # Renomeia as colunas para um padrão comum
+    df = df.rename(columns=settings.RENAME_COLUNAS_ESTADO)
+
+    # Faz a correção dos valores para percentual
     df[['perc_cotas_negras','perc_cotas_indigenas','perc_cotas_pcd']] = (
         df[['perc_cotas_negras','perc_cotas_indigenas','perc_cotas_pcd']]
-        .apply(padronizar_percentual)
+        .apply(sanitize.padronizar_percentual)
     )
-    # SANITIZE
-    df['valor_total_rs'] = moeda_br_para_float(df['valor_total'])
-    # SANITIZE
+
+    # Faz correção do valor total para float
+    df['valor_total'] = sanitize.moeda_br_para_float(df['valor_total'])
+
+    # Passa vagas totais para formato integer
     df['vagas_totais'] = df['vagas_totais'].astype('Int64')
+
+    # 
+    file_io.save_data(df, file_name='estados_processado', format='.parquet', area='processed')
+    file_io.save_data(df, file_name='estados_processado', format='.csv', area='processed')
