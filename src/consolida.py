a0ae581b5c78036901ef5dcb630b6bb1133e2925
Gabriel Ribeiro Bizerril <gabrielribeirob.16@gmail.com>
2026-02-03 13:49:53 -0300

    v3 da tabela data SNIIC

--- src/consolida.py
+++ src/consolida.py
@@ -5,16 +5,18 @@ from config import settings
 from pathlib import Path
 pd.set_option('display.max_columns', None)
 
-DATA_PRINCIPAL_FILE_PATH = '../data/processed/Ações Afirmativas na Política Nacional Aldir Blanc - Análise de Descumprimentos.xlsx'
-DATA_V2                  = '../data/processed/base_dados_09_01_26_14h50.xlsx'
-DATA_V2_ABA              = 'base_dados_09-01-26'
+# DATA_PRINCIPAL_FILE_PATH = '../data/processed/Ações Afirmativas na Política Nacional Aldir Blanc - Análise de Descumprimentos.xlsx'
+# DATA_V2                  = '../data/processed/base_dados_09_01_26_14h50.xlsx'
+DATA_V3                  = '../data/processed/base_dados_09_01_26_19h50.xlsx'
+# DATA_V2_ABA              = 'base_dados_09-01-26'
+DATA_V3_ABA              = 'base_dados_09_01_26_completa'
 GEO_DATA_EST             = 'ibge_est_regioes_5_12_2025_14_49.parquet'
 PLANILHA_ADESAO          = '../data/external/Adesão - Politica Nacional Aldir Blanc.xlsx'
 
-df = pd.read_excel(DATA_PRINCIPAL_FILE_PATH, sheet_name='Resultado Final')
-df_v2 = pd.read_excel(DATA_V2, sheet_name=DATA_V2_ABA)
+# df = pd.read_excel(DATA_PRINCIPAL_FILE_PATH, sheet_name='Resultado Final')
+df_v2 = pd.read_excel(DATA_V3, sheet_name=DATA_V3_ABA)
 
-def transform():
+def transform_deprected():
     # Exclui a coluna de parecer
     df = df.drop(columns=['Conferência/Parecer'])
     df = df.reset_index(drop=True)
